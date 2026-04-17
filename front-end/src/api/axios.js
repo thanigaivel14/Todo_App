4f9c366e775e8da180895d97e3277a76e304ccf6
thanigaivel14 <thanigai905@gmail.com>
2026-04-17 14:04:38 +0530

    url updated

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-
+const url=import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 const API =axios.create({
-          baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+          baseURL: url,
           withCredentials:true
 })
 
